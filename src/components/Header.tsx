2b9ffa736452075758654c28c5bf4665ac7d87bf
gadadeanjali <gadadeanjali677@gmail.com>
2025-12-28 19:31:22 +0530

    added splash screen images ,

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -192,14 +192,15 @@ const sections: Visible[] = [
             {/* Logo */}
             <Link to={CUSTOM_ROUTES.HOME}>
               <div className="flex items-center gap-4">
-                <div className="w-14 h-14 sm:w-12 sm:h-12 md:w-16 md:h-16 rounded-full overflow-hidden">
-                  <LazyLoadImage
-                    src="/favicon.ico"
-                    alt="Logo"
-                    effect="blur"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
+               <div className="w-14 h-14 sm:w-12 sm:h-12 md:w-16 md:h-16 rounded-full bg-white flex items-center justify-center">
+  <LazyLoadImage
+    src="/favicon.png"
+    alt="App Logo"
+    effect="blur"
+    className="w-full h-full object-contain"
+  />
+</div>
+
 
                 <div>
                   <h1 className="text-base sm:text-lg md:text-xl font-bold bg-gradient-to-r from-primary to-primary/70 bg-clip-text text-transparent">
